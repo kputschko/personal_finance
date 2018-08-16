3c501a9b9b38d8d7a5a4ffe8e92b748a23ab227e
Kevin Putschko <kputschko@gmail.com>
2018-08-16 14:53:02 -0400

    New Shiny RMarkdown

--- R/add_rows.R
+++ R/add_rows.R
@@ -7,7 +7,6 @@
 # Hit final button to bind_cols with original table and write_csv to save?
 # Need to migrate mom's table tracking to different location
 
-set_date <- "2018-08"
 
 
 
@@ -37,15 +36,38 @@ type_shortcut <-
   arrange(Type)
 
 
+set_date <- "2018-08"
 set_string <- "15, gro, Meijer, 20.50"
 
+set_string <- "15, gro, Meijer, 20.50/ 15, gro, Meijer, 20.50"
+
+# mod 3 works
+set_string %>% str_count(",")
+set_string %>% str_replace_all(", ", ",")
+
+tibble(i = set_string) %>%
+  separate_rows(i, sep = "/") %>%
+  separate(i, sep = ",",
+           into = c("Date", "Shortcut", "Description", "Amount")) %>%
+  mutate_all(str_trim) %>%
+  mutate(Date = str_c(set_date, Date, sep = "-") %>% as.Date(),
+         Amount = Amount %>% as.numeric()) %>%
+  left_join(type_shortcut, by = "Shortcut") %>%
+  select(Date, Type, Description, Amount)
+
+
+
+
+
+str_count(set_string, ",")
+
+tibble(i = set_string) %>%
+  separate(i,
+           c("Date", "Shortcut", "Description", "Amount"),
+           sep = ", ")
 
 set_string %>%
   str_split_fixed(", ", n = 4) %>%
   as_tibble() %>%
   set_names(c("Date", "Shortcut", "Description", "Amount")) %>%
   bind_cols() %>%
-  mutate(Date = str_c(set_date, Date, sep = "-") %>% as.Date(),
-         Amount = Amount %>% as.numeric()) %>%
-  left_join(type_shortcut) %>%
-  select(Date, Type, Description, Amount)
